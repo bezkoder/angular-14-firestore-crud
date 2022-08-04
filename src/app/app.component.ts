bbf7ccb648afd7c4781824bfcbd18ed09eefe598
tienbku <tien.ngtruong@gmail.com>
2022-08-04 15:05:33 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-14-firestore-crud';
+  title = 'Angular 14 Firestore CRUD';
 }
